bab3b1c8cb4100fcc42c719d77ba3fc851912d72
Yi Zu <yizucodes@gmail.com>
2024-07-27 18:03:17 -0400

    FE: Temp player 1 winning after 5 seconds

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -3,16 +3,17 @@ import './Car.css';
 
 interface CarProps {
   color: string;
+  className?: string;
 }
 
-const Car: React.FC<CarProps> = ({ color }) => {
+const Car: React.FC<CarProps> = ({ color, className }) => {
   return (
-    <div className='car' style={{ background: color }}>
+    <div className={`car ${className}`} style={{ backgroundColor: color }}>
       <div className='roof'></div>
-      <div className='hood' style={{ background: color }}></div>
-      <div className='trunk' style={{ background: color }}></div>
-      <div className='left-mirror' style={{ background: color }}></div>
-      <div className='right-mirror' style={{ background: color }}></div>
+      <div className='hood' style={{ backgroundColor: color }}></div>
+      <div className='trunk' style={{ backgroundColor: color }}></div>
+      <div className='left-mirror' style={{ backgroundColor: color }}></div>
+      <div className='right-mirror' style={{ backgroundColor: color }}></div>
       <div className='left-wheel front'></div>
       <div className='right-wheel front'></div>
       <div className='left-wheel back'></div>
