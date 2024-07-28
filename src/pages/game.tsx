da1ecb6dc34898cc61de9a829af89eb1b833ed6d
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2024-07-28 08:23:20 -0400

    ui_push

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from 'react';
 // import Car from '../components/Car';
 import './game.css';
 import CarCanva from './carCanva';
+interface carData {
+  score :number;
+  speed :number;
+  bestLap : number;
+  rank : number;
+}
 interface CarProps {
   className?: string;
   position?: string;
 }
 
 interface StatusProps {
-  score: number;
-  speed: number;
+  leftData: carData;
+  rightData: carData;
 }
+
 
 const backStart = 'giphy_frame.png';
 const backActive = 'giphy-background.webp';
@@ -67,40 +74,35 @@ const Car: React.FC<CarProps> = ({ className='', position }) => {
   
 };
 
-const Status: React.FC<StatusProps> = ({ score, speed }) => {
+const Status: React.FC<StatusProps> = ({ leftData, rightData}) => {
   return (
     <div className="status">
-      <div>Score: {score}</div>
-      <div>Speed: {speed} mph</div>
+      <div className='flex-horizontal'>
+        <span>Score: {leftData.score}</span>
+        <span>Speed: {leftData.speed}</span>
+      </div>
+      <div className='flex-horizontal'>
+        <span>Score: {rightData.score}</span>
+        <span>Speed: {rightData.speed}</span>
+      </div>
     </div>
   );
 };
 
+
+
 export const Game: React.FC = () => {
-  const [score, setScore] = useState<number>(0);
-  const [speed, setSpeed] = useState<number>(0);
+  const [leftCar, setLeftCar] = useState<carData>({score:0, speed:0, bestLap:0, rank:0});
+  const [rightCar, setRightCar] = useState<carData>({score:0, speed:0, bestLap:0, rank:0});
   const [gameState, setGameState] = useState<'null'|'active'|'finished'>('null');
-  // const [leftCarPosition, setLeftCarPosition] = useState<number>(40); // Adjusted for percentage
-  // const [rightCarPosition, setRightCarPosition] = useState<number>(60); // Adjusted for percentage
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setScore((prev) => prev + 1);
-  //     setSpeed((prev) => Math.min(prev + 1, 100)); // Max speed 100 mph
-  //   }, 100);
-
-  //   return () => clearInterval(interval);
-  // }, []);
 
   const handleStartClick = () => {
-    setSpeed(50); // Initial speed
-    setScore(10);
     setGameState('active');
   };
 
   return (
     <div className='game flex-vertical'>
-      <Status score={score} speed={speed} />
+      <Status leftData={leftCar} rightData={rightCar}/>
       <Road classes={gameState === 'active'?'strip-animate':''} />
       {/* <Car color='red' className={ 'dash car-left'} leftPosition='30%' /> */}
       {/* <Car color='blue' leftPosition='70%' /> */}
