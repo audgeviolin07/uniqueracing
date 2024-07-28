b9e96b9d790a0b1c9848439988709ac8e5a832d5
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2024-07-28 02:08:18 -0400

    stop game

--- src/pages/CarGame.tsx
+++ src/pages/CarGame.tsx
@@ -11,7 +11,8 @@ const CarGame: React.FC = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setWinner('player1'); // Set the winner to 'player1' after 5 seconds
-    }, 5000);
+      setGameState('finished');
+    }, 3000);
 
     return () => clearTimeout(timer); // Cleanup the timer on component unmount
   }, []);
@@ -29,14 +30,17 @@ const CarGame: React.FC = () => {
       }}
     >
       <div style={{ position: 'relative' }}>
-        <Road id='player1' classes={gameState !== 'null'?'stripe-running' : ''} />
+        <Road id='player1' classes={gameState === 'active'?'stripe-running' : ''} />
         <Car color='red' className={winner === 'player1' ? 'dash' : '' } leftPosition='0'/>
       </div>
       {gameState === 'null' && <div className='flex-vertical'>
         <button onClick={startGame}>Start Game</button>
       </div>}
+      {gameState === 'finished' && <div className='flex-vertical'>
+          Winner is {winner}
+      </div>}
       <div style={{ position: 'relative' }}>
-        <Road id='player2' classes={gameState !== 'null'?'stripe-running' : ''} />
+        <Road id='player2' classes={gameState === 'active'?'stripe-running' : ''} />
         <Car color='blue' className={winner === 'player2' ? 'dash' : ''} leftPosition='0'/>
       </div>
     </div>
